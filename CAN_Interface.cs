5960f66f1468c8ea5b6b813cae689809ba335eda
ai237258 <84716284+ai237258@users.noreply.github.com>
2026-03-28 10:07:57 +0900

    Add files via upload

---
+++ CAN_Interface.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UserInterface
+{
+    public interface Can_Interface : IDisposable
+    {
+        event Action<int, byte[], int, long>? MessageReceived;
+        void SendMessage(int id, byte[] msg, int dlc);
+        bool Initialize(int channel);
+        void StartListening();
+    }
+}
+
